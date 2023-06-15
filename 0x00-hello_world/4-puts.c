11a5f5e7e0451e12b3b30de3832334de7d52af1a
Ahmed-1-adel <Ahmedadel123321m@gmail.com>
2023-06-15 05:34:04 -0700

    Hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Description: A C prgoram that prints with puts function.
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Description: A C prgoram that prints with printf function.
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
