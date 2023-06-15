019c20758355830c1b01f0ad48992ba167f42cba
Ahmed-1-adel <Ahmedadel123321m@gmail.com>
2023-06-15 06:38:26 -0700

    Hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * Main - Entry point
- *
- * Description: A C prgoram that prints with printf function.
- *
- * Return: Alawys 0 (Success)
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
