6427f6531beabd2f91e6d0823b4be9ffb546ee89
Ahmed-1-adel <Ahmedadel123321m@gmail.com>
2023-06-16 05:54:38 -0700

    Hello

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,14 +4,16 @@
  * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-	for (i = 0; i < 10; i++)
+	int i;
 
+	for (i = 48; i < 58; i++)
+	{
 		putchar(i);
-
-	putchar("\n");
+	}
+	putchar('\n');
 	return (0);
 }
+
