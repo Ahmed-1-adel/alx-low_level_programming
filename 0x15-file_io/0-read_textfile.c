65eab3e764ab79e42a4f56a8fd50252337b41356
Ahmed-1-adel <ahmedadel123321m@gmail.com>
2023-09-04 05:13:12 -0700

    Hello

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * @filename: text file being read
  * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * 0 when function fails or filename is NULL.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -26,3 +26,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 	return (w);
 }
+
